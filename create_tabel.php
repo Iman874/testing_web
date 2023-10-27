325817c809e06ea677fd1b00925e2f34d52a7cb5
Rosul Iman <117097805+Iman874@users.noreply.github.com>
2023-10-27 21:08:19 +0000

    f

---
+++ create_tabel.php
@@ -0,0 +1,26 @@
+<?php
+    include ('connect.php');
+
+    // Menggunakan database "testing"
+    $conn->select_db("testing");
+
+    // Membuat tabel "table_tes"
+    $sql_create_table = "CREATE TABLE IF NOT EXISTS table_tes (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        tes varchar(1000)
+    )";
+    if ($conn->query($sql_create_table) === TRUE) {
+        echo "Tabel 'table_tes' berhasil dibuat atau sudah ada.<br>";
+    } else {
+        echo "Error dalam pembuatan tabel: " . $conn->error;
+    }
+
+    // Memasukkan data ke dalam tabel
+    $sql_insert_data = "INSERT INTO table_tes (tes) VALUES ('ini adalah tulisan di dalam database')";
+    if ($conn->query($sql_insert_data) === TRUE) {
+        echo "Data berhasil dimasukkan ke dalam tabel.<br>";
+    } else {
+        echo "Error dalam memasukkan data: " . $conn->error;
+    }
+
+?>
