b2eaf6692602ff06d401c8dbe23c1b8dd5e008f1
Rosul Iman <117097805+Iman874@users.noreply.github.com>
2023-10-28 21:30:47 +0000

    export data

---
+++ export_testing.sql
@@ -0,0 +1,7 @@
+-- Ekspor data dari tabel 'table_tes' di database 'testing'
+-- Tanggal: 2023-10-28 21:30:23
+
+INSERT INTO table_tes (id, tes) VALUES ('1', 'ini adalah tulisan di dalam database');
+INSERT INTO table_tes (id, tes) VALUES ('2', 'ini adalah tulisan di dalam database');
+INSERT INTO table_tes (id, tes) VALUES ('3', 'ini adalah tulisan di dalam database');
+INSERT INTO table_tes (id, tes) VALUES ('4', 'ini adalah tulisan di dalam database');
